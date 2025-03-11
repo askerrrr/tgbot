1dfe79b29e249afb413ed032f7a116f783491daa
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-03-11 14:27:26 +0300

    correct checkOrderExists

--- src/database/services/checkOrderExists.js
+++ src/database/services/checkOrderExists.js
@@ -3,7 +3,7 @@ var { db } = require("../db");
 var checkOrderExists = async (userId, orderId) => {
   var collection = (await db).collection("users");
 
-  var document = await collection.findOne({ userId });
+  var document = await collection.findOne(userId);
 
   var order = document.orders.some((e) => e.order.id == orderId);
 
