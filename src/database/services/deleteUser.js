b8f3e54e874615c297f4fb39dc6d64a5017c6792
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-03-13 11:36:50 +0300

    correct deleteUser

--- src/database/services/deleteUser.js
+++ src/database/services/deleteUser.js
@@ -1,7 +1,7 @@
 var { db } = require("../db");
 
 var deleteUser = async (userId) => {
-  var collection = (await db).collection(users);
+  var collection = (await db).collection("users");
 
   var result = await collection.deleteOne({ userId });
 
