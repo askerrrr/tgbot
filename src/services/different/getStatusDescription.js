35ead6bbc4fa188839622730bc2ed22cb80a7261
asker <askersultanov666@gmail.com>
2025-04-05 22:07:06 +0300

    correct getStatusDescription

--- src/services/different/getStatusDescription.js
+++ src/services/different/getStatusDescription.js
@@ -3,26 +3,26 @@ var getStatusDescription = (orderStatus) => {
 
   var { id } = orderStatus;
   switch (id) {
-    case 0:
+    case "0":
       translatedStatus = "Не взят в обработку";
       break;
-    case 1:
+    case "1":
       translatedStatus = "Взят в обработку";
       break;
-    case 2:
+    case "2":
       translatedStatus = "Товар выкуплен";
       break;
-    case 3:
+    case "3":
       translatedStatus = "Товар поступил на склад в китае";
       break;
-    case 4:
+    case "4":
       translatedStatus = "В пути";
       break;
-    case 5:
+    case "5":
       translatedStatus =
         "Ожидает получения по адресу\nСолдатский переулок  8,  подъезд 2,  этаж 8,  кв 98";
       break;
-    case 6:
+    case "6":
       translatedStatus = "Заказ завершен";
       break;
   }
