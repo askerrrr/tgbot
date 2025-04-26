1aec352ef1dfbd2ad3756877808e2895c4429209
asker <askersultanov666@gmail.com>
2025-04-26 16:17:45 +0300

    refactoring getUserData

--- src/bot/services/different/getUserData.js
+++ src/bot/services/different/getUserData.js
@@ -1,9 +1,12 @@
 var { randomBytes } = require("crypto");
 
-var getUserData = async ({ id, first_name = "", username = "" }) => {
+var getUserData = async (data) => {
+  var userId = data.id + "";
+  var userName = data.username ?? "";
+  var firstName = data.first_name ?? "";
   var passwd = randomBytes(5).toString("hex");
 
-  return { passwd, userId: id + "", firstName: first_name, userName: username };
+  return { userId, passwd, userName, firstName };
 };
 
 module.exports = getUserData;
