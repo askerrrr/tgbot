93f691a6add3fd07fa5f639f6c4e0cf7d3209cc0
asker <askersultanov666@gmail.com>
2025-05-01 17:08:36 +0300

    add err handler for chatMember

--- src/bot/chatMember/chatMember.js
+++ src/bot/chatMember/chatMember.js
@@ -4,20 +4,25 @@ var { greetUser } = require("../services/different/greetUser");
 var sendUserDataToServer = require("../services/different/sendUserDataToServer");
 
 var chatMember = async (bot) => {
-  bot.hears("/start", async (ctx) => {
-    var userData = await getUserData(ctx.chat);
+  try {
+    bot.hears("/start", async (ctx) => {
+      var userData = await getUserData(ctx.chat);
 
-    var db = await dbServices();
+      var db = await dbServices();
 
-    var successCreateUser = await db.createUser(userData);
-    var successResponse = await sendUserDataToServer(userData);
+      var successCreateUser = await db.createUser(userData);
+      var successResponse = await sendUserDataToServer(userData);
 
-    if (!successCreateUser || !successResponse) {
-      return ctx.reply("Произошла ошибка, попробуйте позже");
-    }
+      if (!successCreateUser || !successResponse) {
+        return ctx.reply("Произошла ошибка, попробуйте позже");
+      }
 
-    await ctx.reply(greetUser(ctx.chat.id, ctx.chat.first_name));
-  });
+      await ctx.reply(greetUser(ctx.chat.id, ctx.chat.first_name));
+    });
+  } catch (err) {
+    err.location = "create and send new user data";
+    throw err;
+  }
 };
 
 module.exports = { chatMember };
